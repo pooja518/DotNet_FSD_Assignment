00c9e57b95e085e49d1030a580b1db70ce4cdd95
pooja <118118607+pooja518@users.noreply.github.com>
2024-09-10 21:01:56 +0530

    Add files via upload

---
+++ _Course6._Product_CRUD/ApplicationProduct/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using ApplicationProduct.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationProduct.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductDbContext _context;
+        public ProductController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(_context.products.ToList());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            Product obj = _context.products.Find(id);
+            if (obj != null)
+            {
+                return Ok(obj);
+            }
+            else
+            {
+                return NotFound(new {status = "Requested product details does not exist"});
+            }
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(Product product) 
+        {
+            
+             _context.products.Add(product);
+             _context.SaveChanges();
+             return Ok(new { status = "New product details added in server" });
+           
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(Product product) 
+        { 
+            _context.products.Update(product);
+            _context.SaveChanges();
+            return Ok(new { status = "Product details updated in server" });
+        }
+
+        [HttpDelete("(id")]
+        public IActionResult DeleteProductById(int id) 
+        {
+            Product obj = _context.products.Find(id);
+            if (obj != null)
+            {
+                _context.products.Remove(obj);
+                _context.SaveChanges();
+                return Ok(new { status = "product details are deleted from server" });
+            }
+            else
+            {
+                return NotFound(new { status = "Requested product details does not exists" });
+            }
+        }
+    }
+}
---
+++ _Course6._Product_CRUD/ApplicationProduct/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationProduct.Models
+{
+    public class Product
+    {
+        public int productId { set; get; }
+        public string productName { set; get; }
+        public int UnitPrice { set; get; }
+        public int Quantity { set; get; }
+        public string category { set; get; }
+    }
+    //create database
+
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> products { get; set; }
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
